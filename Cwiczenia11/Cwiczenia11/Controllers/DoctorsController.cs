e2b24d1b0c4a67c482404eceb4649d4edb62c809
s19151 <s19151@pjwstk.edu.pl>
2020-05-18 16:31:45 +0200

    Cw11 - Dodanie projektu oraz potrzebnych klas

---
+++ Cwiczenia11/Cwiczenia11/Controllers/DoctorsController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cwiczenia11.Controllers
+{
+    [Route("api/doctors")]
+    [ApiController]
+    public class DoctorsController : ControllerBase
+    {
+    }
+}
---
+++ Cwiczenia11/Cwiczenia11/Models/Prescription.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cwiczenia11.Models
+{
+    public class Prescription
+    {
+    }
+}
